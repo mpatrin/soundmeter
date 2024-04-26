324616233196350151d2bd384d8090b9006e7014
mpatrin <93445033+mpatrin@users.noreply.github.com>
2024-04-26 21:41:35 +0300

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,47 @@
+basic.forever(function () {
+    if (input.soundLevel() < 40) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            `)
+    }
+    if (input.soundLevel() >= 40 && input.soundLevel() < 100) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.soundLevel() >= 100 && input.soundLevel() < 160) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.soundLevel() >= 160 && input.soundLevel() < 210) {
+        basic.showLeds(`
+            . . . . .
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+    if (input.soundLevel() >= 210) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
+})
